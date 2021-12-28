7575b2ac6643361581245020e91f155a26fe2b84
yorkmac002 <litabeach123@gmail.com>
2021-12-28 16:14:57 -0600

    created navbar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,13 @@
+import NavBar from "../components/NavBar";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <h1>Home</h1>
+            </>
+
+    )
+
 };
 
 export default Home;
