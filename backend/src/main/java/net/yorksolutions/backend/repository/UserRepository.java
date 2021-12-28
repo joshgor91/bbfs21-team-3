b8f4d62d2c5f703793acfcd1a86391c0a09847c4
yorkmac039 <b2155907@gmail.com>
2021-12-28 11:26:31 -0600

    CREATED USER REPOSITORY

--- backend/src/main/java/net/yorksolutions/backend/repository/UserRepository.java
+++ backend/src/main/java/net/yorksolutions/backend/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package net.yorksolutions.backend.repository;
 
-public interface UserRepository {
+import net.yorksolutions.backend.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+
 }
