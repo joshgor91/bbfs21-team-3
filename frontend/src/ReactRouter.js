ac4c47906b992f382cb4406f9957ce6e80f777fa
asesever <alexsvrsn@gmail.com>
2021-12-30 10:58:09 -0600

    product details page created, and using navigate to get to page

--- frontend/src/ReactRouter.js
+++ frontend/src/ReactRouter.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Admin from "./pages/Admin";
+import ProductPage from "./pages/ProductPage";
 
 
 
@@ -20,6 +21,7 @@ function ReactRouter() {
             <Route path='/login' element={<Login/>}/>
             <Route path='/register' element={<Register/>}/>
             <Route path='/admin' element={<Admin/>}/>
+            <Route path='/product/:id' element={<ProductPage/>}/>
         </Routes>
     </Router>
 
