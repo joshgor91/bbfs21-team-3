3cb23a9582784fb1b6b9bcb424886f98cc6b8038
yorkmac027 <madryga7@gmail.com>
2021-12-30 10:49:33 -0600

    product form and add

--- frontend/src/ReactRouter.js
+++ frontend/src/ReactRouter.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Admin from "./pages/Admin";
-
+import Shopkeeper from "./pages/Shopkeeper";
 
 
 function ReactRouter() {
@@ -20,6 +20,7 @@ function ReactRouter() {
             <Route path='/login' element={<Login/>}/>
             <Route path='/register' element={<Register/>}/>
             <Route path='/admin' element={<Admin/>}/>
+            <Route path='/shopkeeper' element={<Shopkeeper/>}/>
         </Routes>
     </Router>
 
---
+++ frontend/src/pages/Shopkeeper.js
@@ -0,0 +1,10 @@
+import ShopkeeperForm from "../components/shopkeeper/ShopkeeperForm";
+
+const Shopkeeper = () => {
+    return <>
+    <h1>Shopkeeper</h1>
+        <ShopkeeperForm/>
+    </>
+}
+
+export default Shopkeeper
