db6e60b3f064ddfea8beea28a13891525bb6d2e1
yorkmac039 <b2155907@gmail.com>
2022-01-03 17:30:13 -0600

    put comment in view cart, sentimental code

--- backend/src/main/java/net/yorksolutions/backend/controller/CartController.java
+++ backend/src/main/java/net/yorksolutions/backend/controller/CartController.java
@@ -29,10 +29,15 @@ public class CartController {
         return "success";
     }
 
+
+
     @CrossOrigin
     @GetMapping("/viewCart/{userid}")
     Iterable<CartItem> viewCart(@PathVariable Long userid) {
 
+//        var cartid = cartRepo.findByUserId(userid).get().id;
+//        return cartItemRepo.findAllByCartId(cartid);
+//    }
         var cart = cartRepo.findByUserId(userid).get();
         return cart.viewCartItems();
     }
