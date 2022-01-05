d984ca109c044c3af0702f1de27fcc4ccbbcfe51
yorkmac002 <litabeach123@gmail.com>
2022-01-05 15:14:23 -0600

    created checkout address form

--- frontend/src/components/Checkout/CheckoutForm.js
+++ frontend/src/components/Checkout/CheckoutForm.js
@@ -1,6 +1,41 @@
+import {Button, Col, Form} from "react-bootstrap";
+
+// @JsonProperty
+// public String address1;
+//
+// @JsonProperty
+// public String address2;
+//
+// @JsonProperty
+// public String city;
+//
+// @JsonProperty
+// public String state;
+//
+// @JsonProperty
+// public String zipcode;
+
 
 const CheckoutForm = () => {
-    return <h1>Checkout</h1>
+    return (
+        <Form className="login-register-form">
+        <Form.Group className="mb-3" controlId="formBasicAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control type="text required" placeholder="Address"/>
+            <Form.Label>Address 2</Form.Label>
+            <Form.Control type="text" placeholder="Address 2"/>
+            <Form.Label>City</Form.Label>
+            <Form.Control type="text required" placeholder="City"/>
+            <Form.Label>State</Form.Label>
+            <Form.Control type="text required" placeholder="State"/>
+            <Form.Label>Zip Code</Form.Label>
+            <Form.Control type="text required" placeholder="Zip Code"/>
+        </Form.Group>
+
+        <Col><Button variant="warning" >Checkout</Button></Col>
+    </Form>
+
+    )
 };
 
 export default CheckoutForm;
