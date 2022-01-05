87bfd2a4cf377f82c8412dbc93f71c3246e40aa8
yorkmac002 <litabeach123@gmail.com>
2022-01-05 15:19:08 -0600

    component for checkout completed, needs redux logic

--- frontend/src/components/Checkout/CheckoutForm.js
+++ frontend/src/components/Checkout/CheckoutForm.js
@@ -1,41 +1,33 @@
 import {Button, Col, Form} from "react-bootstrap";
 
-// @JsonProperty
-// public String address1;
-//
-// @JsonProperty
-// public String address2;
-//
-// @JsonProperty
-// public String city;
-//
-// @JsonProperty
-// public String state;
-//
-// @JsonProperty
-// public String zipcode;
+function CheckoutForm() {
 
 
-const CheckoutForm = () => {
-    return (
-        <Form className="login-register-form">
-        <Form.Group className="mb-3" controlId="formBasicAddress">
-            <Form.Label>Address</Form.Label>
-            <Form.Control type="text required" placeholder="Address"/>
-            <Form.Label>Address 2</Form.Label>
-            <Form.Control type="text" placeholder="Address 2"/>
-            <Form.Label>City</Form.Label>
-            <Form.Control type="text required" placeholder="City"/>
-            <Form.Label>State</Form.Label>
-            <Form.Control type="text required" placeholder="State"/>
-            <Form.Label>Zip Code</Form.Label>
-            <Form.Control type="text required" placeholder="Zip Code"/>
-        </Form.Group>
+    function handleSubmit() {
+//    here we will run a side effect that deletes all cart items + then saves order
+    }
 
-        <Col><Button variant="warning" >Checkout</Button></Col>
-    </Form>
+        return (
+            <Form className="login-register-form">
+                <Form.Group className="mb-3" controlId="formBasicAddress">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control type="text required" placeholder="Address"/>
+                    <Form.Label>Address 2</Form.Label>
+                    <Form.Control type="text" placeholder="Address 2"/>
+                    <Form.Label>City</Form.Label>
+                    <Form.Control type="text required" placeholder="City"/>
+                    <Form.Label>State</Form.Label>
+                    <Form.Control type="text required" placeholder="State"/>
+                    <Form.Label>Zip Code</Form.Label>
+                    <Form.Control type="text required" placeholder="Zip Code"/>
+                </Form.Group>
 
-    )
-};
+                <Col><Button variant="warning" onSubmit="handleSubmit">Checkout</Button></Col>
+            </Form>
+
+        )
+    }
+
+
 
 export default CheckoutForm;
