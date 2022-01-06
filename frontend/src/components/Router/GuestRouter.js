938105ffa3e92fae32939739f60e8df460dbe43e
yorkmac002 <litabeach123@gmail.com>
2022-01-06 12:11:17 -0600

    adding my account to all routes taht need it, edit is saving to state, not yet going to db

--- frontend/src/components/Router/GuestRouter.js
+++ frontend/src/components/Router/GuestRouter.js
@@ -7,6 +7,7 @@ import CartPage from "../../pages/CartPage";
 import {Badge, Button, Form, FormControl, Nav, Navbar} from "react-bootstrap";
 import {GiShoppingCart} from "react-icons/gi";
 import Checkout from "../../pages/Checkout";
+import UserPage from "../../pages/UserPage";
 
 
 
@@ -75,6 +76,7 @@ function GuestRouter({cartQuantity}) {
                     <Route path='/shopkeeper' element={<Home/>}/>
                     <Route path='/product/:id' element={<ProductPage/>}/>
                     <Route path='/cart' element={<CartPage/>}/>
+                    <Route path='/myaccount' element={<Home/>} />
                     <Route path='/cart/checkout' element={<Checkout/>}/>
                 </Routes>
             </Router>
