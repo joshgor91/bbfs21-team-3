89909b58feca07e3aa88a7b93e12c87e5990d13a
yorkmac002 <litabeach123@gmail.com>
2022-01-07 16:13:20 -0600

    added email field if used is guest to checkout, created route that passes email, productId and quantity from local storage to backend if guest

--- frontend/src/modules/order.js
+++ frontend/src/modules/order.js
@@ -155,3 +155,38 @@ export function initiateGetOrderHistory() {
         }).catch(error => console.log(error))
     }
 }
+
+export function initiateGuestOrder(email) {
+
+    let cartStorage = JSON.parse(window.localStorage.getItem('cartItems'))
+    const filteredCartStorage = cartStorage.map(cartItem => {
+        return {
+                    "productId": cartItem.id,
+                    "quantity": cartItem.quantity
+        }
+    })
+    return function addGuestOrderSideEffect(dispatch, getState) {
+        dispatch(addingOrder())
+        fetch("http://localhost:8080/api/order/add2", {
+            method: "POST",
+            headers: {
+                'email': email,
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(filteredCartStorage)
+
+        }).then(response => {
+            if (!response.ok)
+                return dispatch(addOrderFailed())
+            response.text().then(text => {
+                if(text==="success"){
+                    console.log("order placed")
+                    dispatch(goToReceipt())
+                    window.localStorage.clear()
+                }else{
+                    dispatch(addOrderFailed())
+                }
+            })
+        }).catch(error => console.log(error))
+    }
+}
