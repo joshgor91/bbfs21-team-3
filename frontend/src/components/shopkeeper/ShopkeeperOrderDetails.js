439cdd157173b509c08eeb2a4046cd950fb40cd0
yorkmac005 <cmdornfeld@gmail.com>
2022-01-10 13:24:47 -0600

    ShopkeeperOrderDetails updated to include more info

--- frontend/src/components/shopkeeper/ShopkeeperOrderDetails.js
+++ frontend/src/components/shopkeeper/ShopkeeperOrderDetails.js
@@ -1,10 +1,11 @@
 import {Card, Col, Modal, Row} from "react-bootstrap";
+import moment from "moment";
 
 
 function ShopkeeperOrderDetails({order, showOrderDetails, setShowOrderDetails}) {
     return (
         <Modal size='lg' show={showOrderDetails} onHide={() => setShowOrderDetails(false)}>
-            <Modal.Header closeButton>Order {order.orderDetails.orderDetailsId}</Modal.Header>
+            <Modal.Header closeButton>Order no. {order.orderDetails.orderDetailsId}</Modal.Header>
             <Modal.Body>
                     {order.orderItems.map((item, i) => {
                         return (
@@ -33,6 +34,35 @@ function ShopkeeperOrderDetails({order, showOrderDetails, setShowOrderDetails}) 
                             </Row>
                         )
                     })}
+                <hr />
+                <Row>
+                    <Col>Order date</Col>
+                    <Col>{moment(order.orderDetails.dateCreated).format("MMM Do YYYY")}</Col>
+                </Row>
+                <Row>
+                    <Col>
+                        Customer Email Address:
+                    </Col>
+                    <Col>
+                        {order.orderDetails.email}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        Customer Address:
+                    </Col>
+                    <Col>
+                        {/*Need to include to orderDetails*/}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        Order total:
+                    </Col>
+                    <Col>
+                        {/*Currently returning as 0*/}
+                    </Col>
+                </Row>
             </Modal.Body>
         </Modal>
     )
